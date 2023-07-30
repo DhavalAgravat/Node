1a38e0f145b799043cfbac16299605a7aec8dca0
DhavalAgravat <123539026+DhavalAgravat@users.noreply.github.com>
2023-07-30 15:23:05 +0530

    use ejs for render method  of response

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
 import express from "express";
 import path from "path";
 
-//* as good code practise we name our  server as 'app' in express
-// const server = express();
 const app = express();
 
-//! Can directly access routes request using built in get,post etc requets in express
-//! first param 'route -- endpoint to be specific', second one is callback function with request, response as params args
+// Setting-up view engine
+app.set("view engine", "ejs");
+
 app.get("/", (request, response) => {
-  //? send method for  response to send string in body
-  // response.send("Hell Yeah");
+  //? Render method to render ejs file
+  //? Create views folder with files with extesion ejs
+  //? in args of render provide file name without extensionto render it
+  // response.render("index");
 
-  //! sendStatusCode -- send response status code for request
-  // response.sendStatus(401);
-
-  //* json -- can send json data file
-  // response.json({ success: true, data: [{ user: "123" }, { user: "1234" }]
-
-  //? sendFile -- can send file
-  //! Cann't send path name of file this way have to use path library
-  // response.sendFile("/index.html");
-  const curPath = path.resolve();
-  response.sendFile(path.join(curPath, "/index.html"));
-
-  // other response methods are
-  // setHeader -- can set header for request
-
-  //! IMPORTANT
-  //! We can chain response methods to do  multiple things at a time
-  // response.sendStatus(500).send("test").json({ data: [] });
+  //* To pass value in ejs file using render method pass second args as object to access passed values using keys
+  response.render("index", { name: "Void" });
 });
-
-//? Example of API -- my first API --  returns dummy data
-// app.get("/getMyData", (request, response) => {
-//   response.json({
-//     name: "Heavenly Demon",
-//     address: "in Infinite Void",
-//     likes: [
-//       "Sweets",
-//       "Revenge",
-//       "Dark Humor",
-//       "Lots of work",
-//       "Stress",
-//       "Beer & Cigarettes",
-//     ],
-//     dislikes: ["humans", "lies", "Dumbsters", "ShitHeads", "Cameleon"],
-//   });
-// });
 
 app.listen(5000, () => {
   console.log("server is running");
